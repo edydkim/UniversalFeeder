0b369b4ac976954cb5bee46a66f4ee8bf2a2affc
Edward D. Kim <me@app-creative.com>
2015-08-18 12:17:04 +0900

    Create MQConnectionFactoryProperties.java

---
+++ src/main/java/org/ufm/common/MQConnectionFactoryProperties.java
@@ -0,0 +1,66 @@
+package org.ufm;
+
+import com.ibm.mq.jms.JMSC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+
+
+@Component
+@ConfigurationProperties(locations = "classpath:application.properties", ignoreUnknownFields = true, prefix = "mq")
+public class MQConnectionFactoryProperties {
+    private static final Logger log = LoggerFactory.getLogger(MQConnectionFactoryProperties.class);
+
+    @NotNull
+    private String hostname;
+    
+    @NotNull
+    private int port = 1000;
+
+    private boolean inMemory = true;
+
+    private boolean pooled = true;
+
+    private int transactionType = JMSC.MQJMS_TP_CLIENT_MQ_TCPIP;
+
+    private String queueManager = "DEFAULT";
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setQueueManager(String queueManager) {
+        this.queueManager = queueManager;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public boolean isInMemory() {
+        return inMemory;
+    }
+
+    public boolean isPooled() {
+        return pooled;
+    }
+
+    public int getTransactionType() {
+        return transactionType;
+    }
+
+    public String getQueueManager() {
+        return queueManager;
+    }
+}
