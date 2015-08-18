7b0807c844306dba1433b5d0dacefe9ec53d1b98
Edward D. Kim <me@app-creative.com>
2015-08-18 12:14:36 +0900

    Create MQConnectionFactoryCreator.java

---
+++ src/main/java/org/ufm/common/MQConnectionFactoryCreator.java
@@ -0,0 +1,22 @@
+package org.ufm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableConfigurationProperties(MQConnectionFactoryProperties.class)
+public class MQConnectionFactoryCreator {
+    private static final Logger log = LoggerFactory.getLogger(MQConnectionFactoryCreator.class);
+
+    @Autowired
+    @Qualifier("MQConnectionFactoryProperties")
+    private MQConnectionFactoryProperties mqConnectionFactoryProperties;
+
+    public MQConnectionFactoryCreator() {
+    }
+}
