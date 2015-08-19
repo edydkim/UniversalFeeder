ae102541ce42bf22253fa429f488f19003ea18b4
Edward D. Kim <me@app-creative.com>
2015-08-19 09:19:55 +0900

    Create RunRule.java

---
+++ src/main/java/org/ufm/drools/RunRule.java
@@ -0,0 +1,52 @@
+package org.ufm.drools;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.*;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class RunRule {
+    private static final Logger logger = LoggerFactory.getLogger(RunRule.class);
+
+    private static KnowledgeBase kbase;
+
+    public static void setup() {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(ResourceFactory.newClassPathResource("basic.drl"), ResourceType.DRL);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+    }
+
+    public static void main(String[] args) {
+        setup();
+
+        StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+        // NOTE: <- KnowledgeRuntimeLogger krLogger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "basic");
+
+        BasicVO vo = new BasicVO();
+        vo.setStringValue("first");
+        vo.setBooleanValue(true);
+        ksession.insert(vo);
+        ksession.fireAllRules();
+        ksession.getObjects().stream().filter(o -> o instanceof RoolVO).forEach(o -> {
+            assertEquals("Done.", ((RoolVO) o).getStringValue());
+            logger.info("Transformed value: " + vo.getStringValue());
+        });
+        // NOTE: <- krLogger.close();
+    }
+
+    private static void assertEquals(String s, String stringValue) {
+        if (!s.equals(stringValue)) throw new IllegalArgumentException("occurred assertEquals violation..");
+    }
+}
