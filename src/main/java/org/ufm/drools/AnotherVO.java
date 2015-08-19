e0d3749e96c82d2f584e530f729437e4cd3031f5
Edward D. Kim <me@app-creative.com>
2015-08-19 15:21:05 +0900

    Create AnotherVO.java

---
+++ src/main/java/org/ufm/drools/AnotherVO.java
@@ -0,0 +1,23 @@
+package org.ufm.drools;
+
+
+public class AnotherVO {
+    private String stringValue;
+    private boolean booleanValue;
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public boolean isBooleanValue() {
+        return booleanValue;
+    }
+
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
+}
